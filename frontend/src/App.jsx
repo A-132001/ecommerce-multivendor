748f8d1fd4026b0784362c8df3389f9e8f2d69f0
reemmohamed74 <reem74mohamed@gmail.com>
2025-05-16 17:49:32 +0300

    Navbar Setting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-// import Navigation from './components/Navbar';
+import Navigation from './components/Navbar';
 import Footer from './components/Footer';
 import StorePage from './pages/StorePage';
 import DashboardPage from './pages/DashboardPage';
@@ -62,7 +62,7 @@ function App() {
   return (
     <Router>
       <AuthProvider>
-        {/* <Navigation /> */}
+        <Navigation />
          <ToastContainer />
         <Routes>
           {publicRoutes.map(({ path, element }) => (
@@ -75,7 +75,7 @@ function App() {
               element={<ProtectedRoute>{element}</ProtectedRoute>}
             />
           ))}
-          {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
         <Footer />
       </AuthProvider>
